91fae404a2bc733db7d398c6aa64a7d99b6bfb88
Genki Katsutani <mm@nibushibu.com>
2024-10-28 23:03:26 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# Riot.js
+
+## How to run
+
+```zsh
+npm install
+npm run start
+```
---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import RawHtml from './components/raw-html.js'
+import RawHtmlWorkaround from './components/raw-html-workaround.js'
+import define from '@riotjs/custom-elements'
+
+define('raw-html', RawHtml)
+define('raw-html-workaround', RawHtmlWorkaround)
